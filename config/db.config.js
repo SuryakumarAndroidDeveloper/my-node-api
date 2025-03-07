56c587cff5bf956b0a084373692cdfcb5b6977ab
Surya Kumar Radhakrishnan <sradhakrishnan@thapovan-inc.com>
2025-03-07 13:56:52 +0530

    localhost

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root', // Replace with your MySQL username
   password: 'root', // Replace with your MySQL password
   database: 'payment_reminder' // Replace with your database name
